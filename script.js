a5eca6f952c46b6b95c633114eb66e5ea14eb326
Courtney Brecht <Courtney Brecht>
2017-04-07 15:38:01 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.sliderImage').slick({
+    arrows: false,
+    dots: true,
+    dotsClass:"slick-dots",
+    autoplay: true,
+    adaptiveHeight: true,
+    autoplaySpeed: 10000,
+  });
+});
